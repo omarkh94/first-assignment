5dc1093031691e35a468d48cac32503f19d91018
omarkh94 <omarkh94@yahoo.com>
2025-05-29 19:52:02 +0300

    finish the first task

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# 29MayAssignment
+### Running the project
+
+you can run my project by installing the packages 
+then use this command dart run
+into your terminal 
---
+++ bin/first_assignment.dart
@@ -0,0 +1,37 @@
+void main() {
+  double? calculate(double? num1, double? num2, String? operation) {
+    // reassign the parameters to zero if it null 
+    num1 = num1 ?? 0.0;
+    num2 = num2 ?? 0.0;
+
+    switch (operation) {
+      case "+":
+        return num1 + num2;
+      case "*":
+        return num1 * num2;
+      case "/":
+        if (num2 != 0.0) {
+          return num1 / num2;
+        }
+        print("num2 can't be zero");
+        return 0.0;
+
+      case "-":
+        return num1 - num2;
+      case "%":
+        if (num2 != 0.0) {
+          return num1 % num2;
+        }
+        print("num2 can't be zero");
+        return 0.0;
+      default:
+        return num1 + num2;
+    }
+  }
+
+  // test cases 
+  print(calculate(10, 5, "/")); // 2.0
+  print(calculate(10, 0, "/")); // 0.0
+  print(calculate(10, 0, "%")); //  0.0
+  print(calculate(10, 2, "%")); //  0.0
+}
